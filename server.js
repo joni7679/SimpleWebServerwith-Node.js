5117766bfb69e7ac7cf0c32d452ee4b5002cbd3f
joni <codingmylife0@gmail.com>
2025-10-01 19:35:58 +0530

    Creating a Simple Web Server with Node.js task complete ...

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = http.createServer((req, res) => {
     fs.readFile(filePath, (err, data) => {
         if (err) {
             res.writeHead(500, { "Content-Type": "text/plain" });
-            res.end("500 - Internal Server Error");
+            res.end("500 -Internal server error");
         } else {
             res.writeHead(200, { "Content-Type": "text/html" });
             res.end(data);
